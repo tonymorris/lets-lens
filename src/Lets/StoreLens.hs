8eb8d5065fa4839e9bb5bf48ea4c2bd677747f6c
Tony Morris <tmorris@tmorris.net>
2015-05-01 15:18:23 +1000

    Store lens

--- src/Lets/StoreLens.hs
+++ src/Lets/StoreLens.hs
@@ -10,6 +10,54 @@ data Store s a =
     (s -> a)
     s
 
+setS ::
+  Store s a
+  -> s -> a
+setS (Store s _) =
+  s
+
+getS ::
+  Store s a
+  -> s
+getS (Store _ g) =
+  g
+
+-- todo; functor, comonad
+
 data Lens a b =
   Lens
     (a -> Store b a)
+
+-- | The get/set law of lenses. This function should always return @True@.
+getsetLaw ::
+  Eq a =>
+  Lens a b
+  -> a
+  -> Bool
+getsetLaw (Lens r) =
+  \a -> 
+    let Store s g = r a 
+    in s g == a
+  
+-- | The set/get law of lenses. This function should always return @True@.
+setgetLaw ::
+  Eq b =>
+  Lens a b
+  -> a
+  -> b
+  -> Bool
+setgetLaw (Lens r) a b =
+  getS (r (setS (r a) b)) == b
+
+-- | The set/set law of lenses. This function should always return @True@.
+setsetLaw ::
+  Eq a =>
+  Lens a b
+  -> a
+  -> b
+  -> b
+  -> Bool
+setsetLaw (Lens r) a b1 b2 =
+  let s = setS (r a)
+  in setS (r (s b1)) b2 == s b2
+  
