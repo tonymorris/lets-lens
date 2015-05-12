ec18de973c4d3df6c0ae7f5ffdf9c12ff9210ecd
Tony Morris <tmorris@tmorris.net>
2015-05-12 17:54:29 +1000

    lens

--- src/Lets.hs
+++ src/Lets.hs
@@ -4,5 +4,6 @@ module Lets(
 
 import Lets.Data as L
 import Lets.GetSetLens as L()
+import Lets.Lens as L()
 import Lets.OpticLens as L()
 import Lets.StoreLens as L()
---
+++ src/Lets/Lens.hs
@@ -0,0 +1,9 @@
+module Lets.Lens where
+
+-- Let's remind ourselves of 
+-- class (Foldable t, Functor t) => Traversable t where
+--   traverse ::
+--     Applicative f => 
+--     (a -> f b)
+--     -> t a
+--     -> f (t b)
